392af8ebf9087aeac7781278e241ed83e4613ee4
michaelgobz <raniahgobz@outlook.com>
2023-03-07 09:10:37 +0000

    express toplevel fucntion

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import { express } from 'express';
+import express from 'express';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
